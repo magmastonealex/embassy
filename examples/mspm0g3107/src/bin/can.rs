a172ed80909dc4eeb55a4059883745446e204e63
Alex Roth <alex@alexroth.me>
2026-02-01 13:17:27 -0500

    Update can example to use async better

--- examples/mspm0g3107/src/bin/can.rs
+++ examples/mspm0g3107/src/bin/can.rs
@@ -1,14 +1,16 @@
 #![no_std]
 #![no_main]
 
-use core::num;
-
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_futures::select::{select, Either};
+use embassy_mspm0::can::{CanRx, CanTx};
+use embassy_mspm0::can::frame::MCanFrame;
 use embassy_mspm0::gpio::Output;
+use embassy_mspm0::mode::Async;
 use embassy_mspm0::{Config, bind_interrupts, can};
 use embassy_mspm0::peripherals::CANFD0;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Channel, DynamicReceiver, DynamicSender};
 use embassy_time::{Duration, Instant, Timer};
 use embedded_can::{Frame, Id, StandardId};
 use {defmt_rtt as _, panic_probe as _};
@@ -25,8 +27,45 @@ bind_interrupts!(struct Irqs {
 //
 // This example also demonstrates bus-off recovery, which can be triggered by shorting the CAN+ and CAN- lines together.
 
+static OUTBOUND: Channel<ThreadModeRawMutex, MCanFrame, 10> = Channel::new();
+
+#[embassy_executor::task]
+async fn periodic_hello(outgoing: DynamicSender<'static, MCanFrame>) {
+    loop {
+        Timer::after_secs(5).await;
+
+        for number in 1u8..10 {
+            let frame =
+                can::frame::MCanFrame::new(Id::Standard(StandardId::new(0x123).unwrap()), &[0x12u8, 0x34, number])
+                    .unwrap();
+            outgoing.send(frame).await;
+            info!("Sent hello frame!");
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn transmitter_mux(mut tx: CanTx<Async>, incoming: DynamicReceiver<'static, MCanFrame>) {
+    loop {
+        let frame = incoming.receive().await;
+        tx.enqueue_frame(&frame).await.expect("no buserror possible");
+    }
+}
+
+#[embassy_executor::task]
+async fn receiver(mut rx: CanRx<Async>, outgoing: DynamicSender<'static, MCanFrame>) {
+    loop {
+        let mut frame = rx.get_frame().await.expect("no buserror possible right now");
+        info!("Received frame: {}", frame);
+        frame.set_id(Id::Standard(StandardId::new(0x0ab).unwrap()));
+        info!("Sending reply... {}", frame);
+        outgoing.send(frame).await;    
+    }
+}
+
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     info!("Hello from the CAN example!");
     let p = embassy_mspm0::init(Config::default());
 
@@ -35,65 +74,30 @@ async fn main(_spawner: Spawner) -> ! {
     let mut _canstb = Output::new(p.PA0, embassy_mspm0::gpio::Level::Low);
 
     // Configure CANFD for a 100kbit/s bitrate.
-    let mut candriver = can::Can::new_async(p.CANFD0, p.PA27, p.PA26, Irqs, can::Config::default()).unwrap();
+    let candriver = can::Can::new_async(p.CANFD0, p.PA27, p.PA26, Irqs, can::Config::default()).unwrap();
+    let (tx, rx, mut status) = candriver.split();
 
-    let mut last_stat_dump = Instant::now();
-    let mut last_sent = Instant::now();
+    spawner.spawn(receiver(rx, OUTBOUND.dyn_sender()).unwrap());
+    spawner.spawn(transmitter_mux(tx, OUTBOUND.dyn_receiver()).unwrap());
+    spawner.spawn(periodic_hello(OUTBOUND.dyn_sender()).unwrap());
 
     loop {
-        match select(candriver.get_frame(), Timer::after(Duration::from_secs(2))).await {
-            Either::First(res) => {
-                let mut frame = res.expect("no buserror");
-                info!("Received frame: {}", frame);
-                frame.set_id(Id::Standard(StandardId::new(0x0ab).unwrap()));
-                info!("Sending reply... {}", frame);
-                if candriver.enqueue_frame(&frame).await.is_err() {
-                    warn!("Frame queue is full!");
+        Timer::after_secs(3).await;
+
+        let errors = status.get_error_counters();
+
+        info!("CAN error counters: {:?}", errors);
+
+        if errors.bus_off {
+            info!("Starting bus-off recovery");
+            match status.recover() {
+                Ok(_) => {
+                    info!("Bus-off recovery completed.");
                 }
-            },
-            Either::Second(_) => {
-                info!("timed out");
-            }
-        }
-
-        Timer::after(Duration::from_secs(2)).await;
-
-        if Instant::now().duration_since(last_stat_dump).as_millis() > 1000 {
-            last_stat_dump = Instant::now();
-
-            let errors = candriver.get_error_counters();
-
-            info!("CAN error counters: {:?}", errors);
-
-            if errors.bus_off {
-                info!("Starting bus-off recovery");
-                match candriver.recover() {
-                    Ok(_) => {
-                        info!("Bus-off recovery completed.");
-                    }
-                    Err(e) => {
-                        warn!("Bus-off recovery failed: {}", e);
-                    }
+                Err(e) => {
+                    warn!("Bus-off recovery failed: {}", e);
                 }
             }
-        }
-
-        // every 5 seconds, send an unsolicited message.
-        if Instant::now().duration_since(last_sent).as_millis() > 5000 {
-
-            for number in 1u8..50 {
-                let frame =
-                    can::frame::MCanFrame::new(Id::Standard(StandardId::new(0x123).unwrap()), &[0x12u8, 0x34, number])
-                        .unwrap();
-                if candriver.enqueue_frame(&frame).await.is_err() {
-                    warn!("Frame queue is full!");
-                } else {
-                    info!("Sent hello frame!");
-                }
-            }
-            
-
-            last_sent = Instant::now();
         }
     }
 }
